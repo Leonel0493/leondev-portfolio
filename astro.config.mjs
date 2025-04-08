08a4c1be771cb1f3ab8deab0cd7a5f27c7851134
Leonel Rivas <leonrivas04@gmail.com>
2025-04-07 19:21:31 -0600

    Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
@@ -8,6 +9,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   integrations: [react()],
   output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
   vite: {
     plugins: [tailwindcss()],
   },
